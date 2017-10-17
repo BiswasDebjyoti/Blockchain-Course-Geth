cd9677dbac7cd3d44c5025187a75a2d4064c59bc
BiswasDebjyoti <debjyoti.biswas.che14@iitbhu.ac.in>
2017-10-17 10:53:21 +0530

    Changed the path

--- bash-geth-scripts/geth-testnet.sh
+++ bash-geth-scripts/geth-testnet.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #  DO NOT FORGET TO REPLACE --testnet if you are using --dev
 
-geth --testnet --identity "MyTestNode" --datadir "./data"  --verbosity "4" --fast
+geth --testnet --identity "MyTestNode" --datadir "/home/debjyoti/Blockchain-Course-Geth/bash-geth-scripts"  --verbosity "4" --fast
